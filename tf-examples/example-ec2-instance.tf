4577c269d03639e3224735ddfdfab0f604e98dc4
vjshar <104772873+vjshar@users.noreply.github.com>
2024-07-15 12:42:46 -0400

    Update example-ec2-instance.tf

--- tf-examples/example-ec2-instance.tf
+++ tf-examples/example-ec2-instance.tf
@@ -1,8 +1,13 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0abcdef1234567890"
+provider "aws" {
+  region = "us-east-2" # Specify your AWS region here
+}
+
+resource "aws_instance" "my_ec2_instance" {
   instance_type = "t2.micro"
-  key_name      = "my-key"
+  ami           = "ami-0862be96e41dcbf74"
+  key_name      = "ubuntu"
+
   tags = {
-    Name = "example-instance"
+    Name = "MyEC2Instance"
   }
 }
