b758fe8c403cf074dc2a0806f377325f49dabb80
vjshar <104772873+vjshar@users.noreply.github.com>
2024-07-15 12:44:50 -0400

    Update example-vpc.tf

--- tf-examples/example-vpc.tf
+++ tf-examples/example-vpc.tf
@@ -1,14 +1,20 @@
-resource "aws_vpc" "main" {
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+
   tags = {
-    Name = "main-vpc"
+    Name = "demo-vpc"
   }
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
+
   tags = {
-    Name = "main-subnet"
+    Name = "demo-subnet1"
   }
 }
